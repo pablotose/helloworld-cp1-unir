13a6a42a346d550e7e7159e535074f2da9f2e987
pablotose <pablotoledosegundo@gmail.com>
2026-01-18 15:52:40 +0100

    Fix coverage v1: cover divide by zero branch

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -63,7 +63,7 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
 
     def test_divide_by_zero_raises_typeerror(self):
-        self.assertRaises(TypeError, self.calc.divide, "1", "0")
+        self.assertRaises(TypeError, self.calc.divide, 1, 0)
         
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
